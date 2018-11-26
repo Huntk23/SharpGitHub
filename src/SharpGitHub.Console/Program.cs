c51174f3307abc335f0217772ce6ace10fa06c98
Kelby Hunt <huntk20@gmail.com>
2018-11-26 06:26:39 -0800

    Initial commit without Issue tracking

---
+++ src/SharpGitHub.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SharpGitHub.ConsoleTest
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new GitHubClient("Simple-GitHub-C#-Client-Test");
+        }
+    }
+}
---
+++ src/SharpGitHub/GitHubClient.cs
@@ -0,0 +1,35 @@
+﻿namespace SharpGitHub
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Reflection;
+    using System.Threading.Tasks;
+
+    public class GitHubClient
+    {
+        public static readonly Uri DefaultGitHubApiUrl = new Uri("https://api.github.com/");
+
+        public GitHubClient(string userAgent)
+            : this(DefaultGitHubApiUrl, userAgent)
+        {
+            
+        }
+
+        public GitHubClient(string baseAddress, string userAgent)
+            : this(new Uri(baseAddress), userAgent)
+        {
+            
+        }
+
+        public GitHubClient(Uri baseAddress, string userAgent)
+        {
+            if (!baseAddress.IsWellFormedOriginalString() && (baseAddress.Scheme != Uri.UriSchemeHttp || baseAddress.Scheme != Uri.UriSchemeHttps))
+            {
+                throw new ArgumentException("The base address used is not a well formed or valid URL.", nameof(baseAddress));
+            }
+        }
+    }
+}
